229cc4c9d74a3c8ea39f663cf8e5a6f92d0970b4
acarun23 <arunchoudhary.oct@gmail.com>
2025-07-27 17:50:33 +0530

    update dictionary and if condition

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+
+colour_code = {'Red': 10, 'Orange': 15, 'Blue': 18}
+print("1.",end =" ")
+print(colour_code)
+
+# access dictionary
+colour_code['Orange'] = 12  # update code fro orange
+del colour_code['Blue']
+print("2.",end =" ")
+print(colour_code)
---
+++ if_condition.py
@@ -0,0 +1,8 @@
+
+age = 12
+if age > 12 and age <= 18:
+    print("Your are teenager")
+elif age <= 12:
+    print("You are child")
+else:
+    print("You are adult")

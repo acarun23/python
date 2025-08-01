60170cedfdfd5686abcfe6cb9437aa8dc60a9522
acarun23 <arunchoudhary.oct@gmail.com>
2025-08-01 18:13:58 +0530

    * added function

---
+++ function.py
@@ -0,0 +1,15 @@
+
+# write function which return length of string and print the string
+
+def name_length(name):
+    print("name = ", name)
+    return len(name)
+
+size = name_length("Arun")
+print("Total character = ", size)
+
+# Addition of two number program
+def add(num1, num2):
+    print("addition = ", num1+num2)
+    
+add(2, 4)
